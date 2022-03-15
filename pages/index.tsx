2f7a54a2295b11b0dcf3313dee3625fb9d784f11
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-03-15 11:55:45 -0300

    Component enunciado

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,21 @@ import QuestaoModel from '../model/questao'
 import RespostaModel from '../model/resposta'
 
 export default function Home() {
-  const questaoTeste = new QuestaoModel(1, 'Melhor Cor', [
+  const questaoTeste = new QuestaoModel(1, 'Melhor Cor ?', [
     RespostaModel.errada('Verde'),
     RespostaModel.errada('Vemelho'),
     RespostaModel.errada('Amarelo'),
     RespostaModel.certa('Preto'),
   ])
-  
+
   return (
-    <Questao valor={questaoTeste} />
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh'
+    }}>
+      <Questao valor={questaoTeste} />
+    </div>
   )
 }
