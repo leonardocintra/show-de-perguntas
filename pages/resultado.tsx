198d9180b515dcafe80b835412c9a5191fcb9502
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-03-20 23:23:46 -0300

    pagina resultado ok

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,5 +1,7 @@
 import styles from '../styles/Resultado.module.css'
 import { useRouter } from "next/router"
+import Estatistica from '../components/Estatistica'
+import Botao from '../components/Botao'
 
 export default function Resultado() {
 
@@ -11,9 +13,12 @@ export default function Resultado() {
     return (
         <div className={styles.resultado}>
             <h1>Resultado Final</h1>
-            <div>{total}</div>
-            <div>{certas}</div>
-            <div>{`${percentual}%`}</div>
+            <div style={{ display: 'flex' }}>
+                <Estatistica texto="Perguntas" valor={total} />
+                <Estatistica texto="Certas" valor={certas} corFundo="#9CD2A4" />
+                <Estatistica texto="Percentual" valor={`${percentual}%`} corFundo="#DE6A33" />
+            </div>
+            <Botao href="/" texto="Tentar novamente" />
         </div>
     )
 }
